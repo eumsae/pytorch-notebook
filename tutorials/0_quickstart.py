6f935089d89bffdf880210cb1c1a6bb892cd87e4
winstr <kimtmdgus22@gmail.com>
2022-12-30 02:34:51 +0000

    Added tutorials/0_quickstart.py

---
+++ tutorials/0_quickstart.py
@@ -0,0 +1,92 @@
+import torch
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class NeuralNet(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_stack = nn.Sequential(
+            nn.Linear(28*28, 512),  # img size: 28x28
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10))  # classes: 10
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_stack(x)
+        return logits
+
+
+def train(model, dataloader, loss_fn, optimizer):
+    model.train()  # set the model to training mode
+    n_samples = len(dataloader.dataset)  # the num of samples
+    for batch_i, (X, y) in enumerate(dataloader):
+        X, y = X.to(DEVICE), y.to(DEVICE)
+        preds = model(X)
+        loss = loss_fn(preds, y)
+        optimizer.zero_grad()  # init grads to 0
+        loss.backward()
+        optimizer.step()
+        if batch_i % 100 == 0:
+            loss, prog = loss.item(), batch_i * len(X)
+            print(f"Loss: {loss:>7f}, Progress: {prog:>5d}/{n_samples:>5d}")
+
+
+def test(model, dataloader, loss_fn):
+    model.eval()  # set the model to evaluation mode
+    n_samples = len(dataloader.dataset)
+    test_loss, correct = 0, 0
+    with torch.no_grad():  # fix grads
+        for X, y in dataloader:
+            X, y = X.to(DEVICE), y.to(DEVICE)
+            preds = model(X)
+            loss = loss_fn(preds, y)
+            test_loss += loss.item()
+            correct += (preds.argmax(1) == y).type(torch.float).sum().item()
+    acc = correct / n_samples
+    avg_loss = test_loss / n_samples
+    print(f"[Test] acc: {acc:>7f}, avg_loss: {avg_loss:>7f}")
+
+
+def __train_test():
+
+    # model
+    model = NeuralNet().to(DEVICE)
+
+    # hyper-parameters
+    lr = 1e-3  # learning rate, 0.001
+    epochs = 5
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(params=model.parameters(), lr=lr)
+    batch_size = 64
+
+    # data
+    training_data = datasets.FashionMNIST(
+        root="data",train=True, download=True, transform=ToTensor())
+    test_data = datasets.FashionMNIST(
+        root="data", train=False, download=True, transform=ToTensor())
+    
+    # data loaders
+    train_dataloader = DataLoader(training_data, batch_size)
+    test_dataloader = DataLoader(test_data, batch_size)
+
+    # train, test
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1} -----")
+        train(model, train_dataloader, loss_fn, optimizer)
+        test(model, test_dataloader, loss_fn)
+    print("Done.")
+
+
+if __name__ == "__main__":
+    __train_test()
